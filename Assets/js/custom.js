1073287556499743eb302e6f44ef9a6839bf12f5
Shrabon Talukder <shrabontalukder425@gmail.com>
2023-11-19 10:47:03 +0600

    Change Some CSS

--- Assets/js/custom.js
+++ Assets/js/custom.js
@@ -58,21 +58,3 @@ var swiper = new Swiper(".studentResources", {
     },
   },
 });
-// Title
-var textWrapper = document.querySelector('.ml6 .letters');
-textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
-
-anime.timeline({loop: true})
-  .add({
-    targets: '.ml6 .letter',
-    translateY: ["1.1em", 0],
-    translateZ: 0,
-    duration: 2000,
-    delay: (el, i) => 50 * i
-  }).add({
-    targets: '.ml6',
-    opacity: 0,
-    duration: 2000,
-    easing: "easeOutExpo",
-    delay: 500
-  });
